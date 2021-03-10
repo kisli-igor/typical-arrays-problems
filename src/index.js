4c0b323c07c9140f54b66fef9e92ceffcc645762
kisli-igor <kisiigor@gmail.com>
2021-03-10 17:50:24 +0200

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length){
+      let min = array[0];
+      for (let c in array){
+        if (min > array[c]) min = array[c];
+      }
+      return min;
+    } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length){
+      let max = array[0];
+      for (let c in array){
+        if (max < array[c]) max = array[c];
+      } return max;
+    }
+    else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length){
+      let avg = 0;
+      for (let c in array){
+        avg += array[c];
+      }
+      return +(avg / array.length).toFixed(2);
+    }else return 0;
 }
